3012af1e60d2e1fd0b12668035d36c5a412e2278
Your Namejudsonfisher <judsonefisher@gmail.com>
2017-09-18 09:38:39 -0500

    done

--- app.js
+++ app.js
@@ -38,58 +38,29 @@ $(document).ready(function(event) {
 
   database.ref().on("child_added", function(snapshot) {
 
-  var sv = snapshot.val();
-
-    console.log(sv.name);
-    console.log(sv.destination);
-    console.log(sv.frequency);
-    console.log(sv.firstTrain);    
-
-    //--------------------------//
+    var sv = snapshot.val();   
 
     var tFrequency = sv.frequency;
-
-    // Time is 3:30 AM
     var firstTime = sv.firstTrain;
 
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
-
     // Current Time
     var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
     // Difference between the times
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-
     // Time apart (remainder)
     var tRemainder = diffTime % tFrequency;
-    console.log(tRemainder);
-
     // Minute Until Train
     var tMinutesTillTrain = tFrequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
     // Next Train
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
-    trainArrival = moment(nextTrain).format("hh:mm");
+    var trainArrival = moment(nextTrain).format("hh:mm");
 
-    // ------------------------- //
+    var entry = "<tr><td>" + sv.name + "</td><td>" + sv.destination + "</td><td>" + sv.frequency + "</td><td>" + trainArrival + "</td><td>" + tMinutesTillTrain + "</td></tr>";
+    $("#table").append(entry);
 
-    var entry = $("#table").append ("<tr><td>" + sv.name + "</td><td>" + sv.destination + "</td><td>" + sv.frequency + "</td><td>" + trainArrival + "</td><td>" + tMinutesTillTrain + "</td></tr>");
-
-    console.log(entry);
+  });
 
 });
-
-});
-
-
-
-// variable captures now 
-
-// var minutes away 
